ac08a4239947da552a005bafda0bbec4c1852cba
midnightsea <kevin.charles.van@gmail.com>
2020-05-09 14:40:42 -0700

    feature: add search bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-
 import Layout from "../containers/layout";
 import SEO from "../components/seo";
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-
+import Image from "../components/image";
 import Layout from "../containers/layout";
 import SEO from "../components/seo";
 
@@ -10,6 +10,9 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
   </Layout>
 );
 
